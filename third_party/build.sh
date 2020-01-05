76faae21fcc72d0972528a7d6471df0e8746fb91
ywl <astralrovers@outlook.com>
2020-01-05 15:27:09 +0800

    protoc,libuv不生成动态库，程序编译不指定静态编译，第三方库使用静态库，系统自带使用动态库

--- third_party/build.sh
+++ third_party/build.sh
@@ -2,36 +2,41 @@
 install_dir=$(pwd)/../
 echo "$install_dir"
 
+# 安装libuv
+cd libuv
+./autogen.sh
+./configure --prefix=$install_dir --enable-shared=no
+make
+make install
+cd ..
+
+# 安装protobuf
+cd protobuf
+./autogen.sh
+./configure --prefix=$install_dir --enable-shared=no
+make
+make install
+cd ..
+
 # 安装easylogger
 cd c-log
 make
 make MAKE_INSTALL_PREFIX=$install_dir install
+cd ..
 
 # 安装util_pthread
-cd ../util_pthread
+cd util_pthread
 make
 make MAKE_INSTALL_PREFIX=$install_dir install
+cd ..
 
 # 安装json
-cd ../json
+cd json
 cp single_include/* $install_dir/include -rf
-
-# 安装libuv
-cd ../libuv
-./autogen.sh
-./configure --prefix=$install_dir --enable-static
-make
-make install
-
-# 安装protobuf
-cd ../protobuf
-./autogen.sh
-./configure --prefix=$install_dir
-make
-make install
+cd ..
 
 # 安装SQLITE_CPP
-cd ../SQLiteCpp
+cd SQLiteCpp
 mkdir build -p
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=$install_dir
@@ -42,7 +47,6 @@ cd sqlite3
 cp libsqlite3.a $install_dir/lib/ -f
 cd ../../
 cp sqlite3/sqlite3.h $install_dir/include/SQLiteCpp/ -f
-
 cd ../
 
 mkdir ../include/sole/ -p
