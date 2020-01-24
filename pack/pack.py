20f1c9de30034a92c031d4d0863b1ae14aec5989
ywl <astralrovers@outlook.com>
2020-01-24 22:21:33 +0800

    修复打包脚本缺陷(1.选项示例错误，2.历史记录加载写入错误)

--- pack/pack.py
+++ pack/pack.py
@@ -5,6 +5,7 @@
 import os
 import json
 import time
+import re
 from optparse import OptionParser
 from collections import namedtuple
 
@@ -18,7 +19,7 @@ parser.add_option("-v", "--version", dest="release_version", type="string", defa
                   help="""
 soft release version. 
 将记录到release-version.json，默认值将使用最新值.
-example: --verion=v1.0.0 or -v=v1.0.0""")
+example: --verion=v1.0.0 or -vv1.0.0""")
 
 # action=""store是可选的，表示将会存储到options里面
 parser.add_option("-b", "--build",
@@ -30,41 +31,62 @@ parser.add_option("-b", "--build",
 3 --> [make clean], 
 4 -- >[make && run], 
 5 --> [make clean && make && run]
-example(default): --build=4 or -b=4""")
+example(default): --build=4 or -b4""")
 
 (options, args) = parser.parse_args()
 
 
 def read_last_release_version():
     """从历史读取最新版本号"""
+
+    print("读取历史版本号")
+
     if not os.path.exists("release_version.json"):
         return options.release_version
 
     with open("release_version.json", "r", encoding="utf8") as f:
         try:
             versions = json.load(f)
-            return versions["release_version"][-1][-1]
+            return versions["release_versions"][-1][-1]
         except Exception:
+            # 异常时，使用输入的版本号
             return options.release_version
 
 
 def save_release_version(release_version):
-    with open("release_version.json", "w+", encoding="utf8") as f:
+    # 记录历史版本号
+
+    with open("release_version.json", "r+", encoding="utf8") as f:
         versions = dict()
         try:
             versions = json.load(f)
             # 如果当前版本和历史最新版本号相同则只对时间进行更新
-            # TODO:如果输入的版本号小于历史版本号则报错
-            if versions["release_version"][-1][-1] == release_version:
-                versions["release_version"][-1][0] = time.asctime(time.localtime(time.time()))
+            if versions["release_versions"][-1][-1] == release_version:
+                versions["release_versions"][-1][0] = time.asctime(time.localtime(time.time()))
             else:
                 versions["release_versions"].append([time.asctime(time.localtime(time.time())), release_version])
 
-        except Exception:
+        except Exception as e:
+            print("解析版本记录失败 : ", e)
+            print("重置记录处理，请确认历史版本正确记录")
             versions["release_versions"] = list()
             versions["release_versions"].append([time.asctime(time.localtime(time.time())), release_version])
 
+        f.seek(0)
+        f.truncate()
         json.dump(versions, f, sort_keys=True, indent=4)
+
+
+def check_build_version(old_version, new_version):
+    print("检查版本号")
+
+    if not re.match(r'[vV](0|[1-9][0-9]*.?)\.(0|[1-9][0-9]*.?)\.(0|[1-9][0-9]*.?)$', new_version):
+        print("%s 版本号格式错误，v不区分大小写，以0开头的版本好只能有一个数字." % new_version)
+        exit(-1)
+
+    if old_version.lower() > new_version.lower():
+        print("新版本号错误，不能比历史最近版本号还小，old:%s, new:%s" % (old_version, new_version))
+        exit(-1)
 
 
 def package_software(release_version):
@@ -116,9 +138,15 @@ def clean_make_and_run(release_version):
 
 
 def main():
-    # 默认情况下读取历史版本号
-    if options.release_version == _DEF_VERSION:
-        version = read_last_release_version()
+    # 读取历史版本号
+    version = read_last_release_version()
+
+    # 校验版本号
+    check_build_version(version, options.release_version)
+    
+    # 默认情况下使用版本号
+    if options.release_version.lower() != _DEF_VERSION:
+        version = options.release_version.lower()
 
     # 记录版本号
     save_release_version(version)
